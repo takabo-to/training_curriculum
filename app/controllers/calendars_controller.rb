7129e6987e57c55c207b608f060379e30e1c97a7
takabo-to <hsp.1king@icloud.com>
2021-12-28 13:51:16 +0900

    コンフリクト

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -18,7 +18,11 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
+<<<<<<< HEAD
   def getWeek
+=======
+  def get_week
+>>>>>>> parent of ac9e9d3 (Merge pull request #8 from takabo-to/second_training)
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
@@ -34,7 +38,11 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+<<<<<<< HEAD
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+=======
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+>>>>>>> parent of ac9e9d3 (Merge pull request #8 from takabo-to/second_training)
       @week_days.push(days)
     end
 
